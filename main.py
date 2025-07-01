9510cec9f45ba733f7773ba25b0d33542ebf9d5e
Deadskinmask811 <cstover811@gmail.com>
2025-07-01 19:04:25 -0400

    added reading book path from command line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_of_words
 from stats import get_num_of_characters
 from stats import dict_to_list
+import sys
 
 def get_book_text(filePath):
         bookContents = ""
@@ -14,7 +15,12 @@ def sort_on(items):
     return items["count"]
 
 def main():
-    numWords = get_num_of_words(get_book_text("books/frankenstein.txt"))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1] 
+    numWords = get_num_of_words(get_book_text(filepath))
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
